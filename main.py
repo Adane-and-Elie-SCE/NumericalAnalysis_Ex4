0b73c71d6615f0bd85b70926482672c99c6cb5cf
Adane <adaneadgo1@gmail.com>
2021-05-18 10:59:37 +0300

    save changes

--- main.py
+++ main.py
@@ -7,29 +7,34 @@ from sympy.utilities.lambdify import lambdify
 
 def Drive():
     x = sp.symbols('x')
-    pol = 4*x**4 + -8*x**3 + x**2 - 3*x + 9
-    rng = (-2, 2)
+    func = 4*x**4 + -8*x**3 + x**2 - 3*x + 9
+    big_range = (-10, 10)
+    little_range = create_range(big_range, lambdify(x, func))
     eps = 0.0001
 
-    print("p(x) = ", pol, ",Range = ", rng, ", epsilon = ", eps)
+    print("p(x) = ", func, ",Range = ", little_range, ", epsilon = ", eps)
     choice = input("[1] Bisection\n[2] Newton-Raphson\n[3] secant\nPick a Method:")
 
     if choice == '1':
-        Bisection_Method(lambdify(x, pol), rng, eps)
+        Bisection_Method(lambdify(x, func), little_range, eps)
 
     elif choice == '2':
-        sol = Newton_Raphson(lambdify(x, pol), rng, eps)
+        sol = Newton_Raphson(lambdify(x, func), little_range, eps)
 
     elif choice == '3':
-        sol = secant_method(lambdify(x, pol), rng, eps)
+        sol = secant_method(lambdify(x, func), little_range, eps)
     else:
         print("Invalid choice")
-
     print(sol)
 
-def create_range(f):
-    for i in range(-10, 10):
 
+def create_range(big_range, f):
+    left_bound, right_bound = big_range
+    a, b = left_bound, left_bound + 1
+    while f(a) * f(b) >= 0 and b <= right_bound:
+        a += 0.1
+        b += 0.1
+    return [a, b]
 
 
 def Bisection_Method(f, rng, eps):
@@ -42,9 +47,6 @@ def Bisection_Method(f, rng, eps):
             b = c
 
     x = (a + b)/2
-
-
-
 
 
 def Newton_Raphson(pol, rng, eps):
