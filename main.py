8bf0fa9e5dde6c5e254daa497b57194d20993f18
Adane <adaneadgo1@gmail.com>
2021-05-19 20:33:38 +0300

    save changes

--- main.py
+++ main.py
@@ -1,41 +1,82 @@
 #  Adane Adgo - 315721969
 #  Elie Bracha -
+import math
 
 import sympy as sp
 from sympy.utilities.lambdify import lambdify
 
 
-def Drive(func, big_range, epsilon, step):
+def Drive(pol, big_range, epsilon, step):
 
-    print("f(x) = ", func, ",Range = ", big_range, ", epsilon = ", epsilon)
+    print("f(x) = ", pol, ",Range = ", big_range, ", epsilon = ", epsilon)
+    Solver(pol, big_range, epsilon, step, method_picker())
+
+
+def method_picker():
     choice = input("[1] Bisection\n[2] Newton-Raphson\n[3] secant\nPick a Method:")
 
     if choice == '1':
-        method = Bisection_Method
+        print("Bisection Method")
+        return Bisection_Method
 
     elif choice == '2':
-        method = Newton_Raphson
+        print("Newton_Raphson")
+        return Newton_Raphson
 
     elif choice == '3':
-        method = secant_method
-
-    else:
-        print("Invalid choice")
-        return
-
-    solver(func, big_range, epsilon, step)
+        print("secant_method")
+        return secant_method
 
 
-def solver(unc, big_range, epsilon, step, method):
+def Solver(pol, big_range, epsilon, step, method):
+
     left_bound, right_bound = big_range
-    a = left_bound, b = left_bound + step
+    f = lambdify(x, pol)
+    df = lambdify(x, sp.diff(pol))
 
-    while b<=
+    #  function
+    a, b = left_bound, left_bound + step
+    while b <= right_bound:
+
+        if f(a) * f(b) < 0:
+            solution = method(f, (a, b), epsilon)
+            sol, iterations = solution
+            if solution is not None:
+                print("x = " + str(sol) + ", number of iteration: " + str(iterations))
+        a += step
+        b += step
+
+    #  derivative
+    a, b = left_bound, left_bound + step
+    while b <= right_bound:
+
+        if df(a) * df(b) < 0:
+            solution = method(f, (a, b), epsilon)
+            sol, iterations = solution
+            if solution is not None and abs(f(sol)) < epsilon:
+                print("x = " + str(sol) + ", number of iterations : " + str(iterations))
+        a += step
+        b += step
 
 
+def Bisection_Method(f, little_range, epsilon):
+    a, b = little_range
+    k = math.ceil(- math.log(epsilon/(b - a), math.e) / math.log(2, math.e))
+    counter = 0
 
-def Bisection_Method(pol, little_range, eps):
-    return 1
+    while abs(b - a) > epsilon:
+        c = (a + b) / 2
+
+        if f(a) * f(c) > 0:
+            a = c
+        else:
+            b = c
+
+        counter += 1
+
+    c = (a + b) / 2
+    if counter <= k:
+        return c, counter
 
 
 def Newton_Raphson(pol, little_range, eps):
@@ -45,12 +86,12 @@ def Newton_Raphson(pol, little_range, eps):
 def secant_method(pol, little_range, eps):
     return True
 
-
+1
 x = sp.symbols('x')
-user_func = 4*x**3 + -8*x**3 + x**2 - 3*x + 9
+user_func = x**4 + x**3 + -3*x**2
 user_range = (-10, 10)
 user_epsilon = 0.0001
 user_step = 0.1
 
 
-# Drive(user_func, user_big_range, user_epsilon, user_step)
+Drive(user_func, user_range, user_epsilon, user_step)
